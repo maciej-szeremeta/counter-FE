d46cc6261f484bd597bc5f5ff0c72da0a9271270
Maciej-Szeremeta <maciej.szeremeta@o2.pl>
2022-09-28 23:02:33 +0200

    Added Layout Main and Create HomeView

---
+++ src/views/HomeView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Main, } from '../components';
+import styles from './HomeView.module.css';
+
+export function HomeView() {
+  return (
+    <Main>
+      HomeView
+    </Main>
+  );
+};
