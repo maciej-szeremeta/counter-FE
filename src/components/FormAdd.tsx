b4fd1cc6cbd8b96d69ff083e5ff7d0675b75d609
Maciej-Szeremeta <maciej.szeremeta@o2.pl>
2022-10-18 23:05:44 +0200

    Added useMutation in UserForm

--- src/components/FormAdd.tsx
+++ src/components/FormAdd.tsx
@@ -1,81 +1,122 @@
-import React, { ChangeEvent, FormEvent, useEffect, useState, } from 'react';
-import { QueryObserverResult, RefetchOptions, RefetchQueryFilters, useQuery, } from 'react-query';
-import { useDispatch, useSelector, } from 'react-redux';
-import { GetOneUsersRes, } from 'types';
-import { RootState, } from '../store';
+import React, { FormEvent, useState, } from 'react';
+import { useMutation, useQueryClient, } from 'react-query';
+import { useDispatch, } from 'react-redux';
 import { apiUrl, } from '../config/api';
 import { Button, Input, } from './common';
 import styles from './FormAdd.module.css';
 import { openForm, } from '../features/open/openSlice';
-import { setUserId, } from '../features/user/userSlice';
 
 interface Props{
   header: string;
   subheader?: string;
-  refetch: <TPageData>(options?: RefetchOptions & RefetchQueryFilters<TPageData>) => Promise<QueryObserverResult<unknown, unknown>>;
 }
-export function FormAdd({ header, subheader, refetch, }: Props) {
+export function FormAdd({ header, subheader, }: Props) {
 
-  const { userId, } = useSelector((state:RootState) => 
-    state.user);
+  // const { userId, } = useSelector((state:RootState) => 
+  //   state.user);
   
   const dispatch = useDispatch();
   
   const [ email, setEmail, ] = useState('');
   const [ pwd, setPwd, ] = useState('');
-  const [ role, setRole, ] = useState('');
-  const [ errors, setError, ] = useState([]); 
+  const [ errorValid, setErrorValid, ] = useState([]); 
+  const [ errorIsExist, setErrorIsExist, ] = useState([]); 
   
-  useQuery<GetOneUsersRes, Error>(
-    [ 'user', ], async ():Promise<GetOneUsersRes> => {
-      const res = await fetch(
-        `${apiUrl}/user/${userId}`, {
+  // useQuery<GetOneUsersRes, Error>(
+  //   [ 'user', ], async ():Promise<GetOneUsersRes> => {
+  //     const res = await fetch(
+  //       `${apiUrl}/user/${userId}`, {
+  //         credentials: 'include',
+  //       }
+  //     );
+  //     if (!res.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     const data = await res.json();
+  //     if (userId) {
+  //       setEmail(data.user.email);
+  //       setRole(data.user.role);
+  //     }
+  //     return data;
+  //   }
+  // );
+  const queryClient = useQueryClient();
+
+  const { isLoading, isSuccess, mutate, } = useMutation(
+  
+    async () => {
+      const req = await fetch(
+        `${apiUrl}/user/register-user`, {
+          method     : 'POST',
+          headers    : { 'Content-Type': 'application/json', },
           credentials: 'include',
+          body       : JSON.stringify({ email, pwd, }),
         }
       );
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
+      if (req.status === 400) {
+        setErrorValid((await req.json()).message);
       }
-      const data = await res.json();
-      if (userId) {
-        setEmail(data.user.email);
-        setRole(data.user.role);
+      else if (req.status === 409) {
+        setErrorIsExist((await req.json()).message);
       }
-      return data;
+ 
+      return req.json();
+      
+    }, {
+      onMutate: addUser => {
+        queryClient.setQueryData(
+          [ 'user', ], addUser
+        );
+        setErrorValid([]);
+      },
+      onSuccess: () => {
+        setEmail('');
+        setPwd('');
+        setErrorValid([]);
+        setErrorIsExist([]);
+        queryClient.invalidateQueries('user');
+      },
     }
   );
- 
-  const addUser = async (e:FormEvent) => {
+
+  const handleSubmit = (e:FormEvent) => { 
     e.preventDefault();
-
-    const req = await fetch(
-      `${apiUrl}/user/register-user`, {
-        method     : 'POST',
-        headers    : { 'Content-Type': 'application/json', },
-        credentials: 'include',
-        body       : JSON.stringify({ email, pwd, }),
-      }
-    );
-
-    const data = await req.json();
-
-    if (data.statusCode === 400) {
-      setError(data.message);
-    }
-    else {
-      setEmail('');
-      setPwd('');
-      setError([]);
-      refetch();
-    }
+    mutate();
   };
 
-  const saveEditUser = (e: FormEvent) => {
-    e.preventDefault();
-    console.log('save edit');
-  };
+  // const addUser = async (e:FormEvent) => {
+  //   e.preventDefault();
 
-  return (
+  //   const req = await fetch(
+  //     `${apiUrl}/user/register-user`, {
+  //       method     : 'POST',
+  //       headers    : { 'Content-Type': 'application/json', },
+  //       credentials: 'include',
+  //       body       : JSON.stringify({ email, pwd, }),
+  //     }
+  //   );
+
+  //   const data = await req.json();
+
+  //   if (data.statusCode === 400) {
+  //     setError(data.message);
+  //   }
+  //   else {
+  //     setEmail('');
+  //     setPwd('');
+  //     setError([]);
+  //     mutate();
+  //   }
+  // };
+
+  // const saveEditUser = (e: FormEvent) => {
+  //   e.preventDefault();
+  //   console.log('save edit');
+  // };
+  return (<>
+    {isLoading && <p>Zapisywanie..</p>}
+    {isSuccess && <p>Dodany ...</p>}
+    {errorIsExist.length === 0 ? null : <p>{errorIsExist[ 0 ]}</p> }
     <form className={styles.form}>
       <div className={styles.headerContainer}>
         <h1 className={styles.header}>{header}</h1>
@@ -87,17 +128,17 @@ export function FormAdd({ header, subheader, refetch, }: Props) {
         value={email}
         placeholder='Email'
         handleChange={setEmail}
-        error={{ error: errors, valid: 'email', }} />
-      {!userId && <Input
+        error={{ error: errorValid, valid: 'email', }} />
+      <Input
         type='password'
         name='pwd'
         value={pwd}
         placeholder='Hasło'
         handleChange={setPwd}
-        error={{ error: errors, valid: 'hasło', }}
-      />}
+        error={{ error: errorValid, valid: 'hasło', }}
+      />
 
-      {userId && <div className={styles.radio}>
+      {/* {userId && <div className={styles.radio}>
         <label
           htmlFor='admin'
           className={styles.radioLabel}>
@@ -126,23 +167,23 @@ export function FormAdd({ header, subheader, refetch, }: Props) {
               setRole(e.target.value)} />
           User
         </label>
-      </div>}
+      </div>} */}
 
       <div className={styles.buttonGroup}>
         <Button
           text='Anuluj'
           type='button'
           handleClick={() => {
-            dispatch(setUserId(''));
             dispatch(openForm());
           }}
         />
         <Button
-          text={!userId?'Dodaj':'Zapisz'}
+          text='Zapisz'
           type='submit'
-          handleClick={!userId?addUser:saveEditUser}
+          handleClick={ handleSubmit}
         />
       </div>
     </form>
+  </>
   );
 };
